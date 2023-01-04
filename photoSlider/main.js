d4de1f19ce3f614fe36dfa5ac958eae29cfe6526
serhiihrytsunyk <serhii.hrytsunyk@gmail.com>
2023-01-04 18:29:33 +0200

    working buttons

--- photoSlider/main.js
+++ photoSlider/main.js
@@ -1,30 +1,30 @@
-const imgWidth = 800;
+const imgWidth = 1600;
 const imgCount = 10;
 const jpg = 'jpg';
 const imgDirect = 'imgs';
 const imgTotalWidth = imgWidth * imgCount;
 
-const images = document.querySelector('.imgs');
+const images = document.querySelector('.images');
 
 for (let i = 0, filename = 1; i < imgCount; i++, filename++) {
 let img = document.createElement('img');
     img.src = `${imgDirect}/${filename}.${jpg}`;
     images.appendChild(img);
-};
+}
 
 
-let position = 0;
+let position = 0
 
-document.querySelector('.arrow_left').onclick =() => {
-    if (position < 0) {
+document.querySelector('.arrow_left').onclick = () => {
+    if (position > 0) {
         position-=imgWidth;
         images.style.marginLeft = -position +'px';
     }
 }
 
 document.querySelector('.arrow_right').onclick =() => {
-    if (position > imgWidth) {
+    if (position < imgTotalWidth) {
         position+=imgWidth;
-        images.style.marginRight = -position +'px';
+        images.style.marginLeft = -position +'px';
     }
 }
