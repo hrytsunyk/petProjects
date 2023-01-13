d1bdd1dcabbf19437636d506472f69c1095c9b76
serhiihrytsunyk <serhii.hrytsunyk@gmail.com>
2023-01-14 01:05:27 +0200

    one more time

--- training/recursion.js
+++ training/recursion.js
@@ -1,35 +1,65 @@
 
     const back_btn = document.createElement('button');
-    back_btn.innerHTML = 'back';
+    back_btn.innerHTML = 'previous';
     const next_btn = document.createElement('button');
     next_btn.innerHTML = 'next';
     document.body.append(back_btn, next_btn)
 
 function getElement(elDOM) {
-
     let children = elDOM.children;
+    let j = 0;
+    for (const child of children) {
 
-    if (children.length !== 0) {
-        for (const child of children) {
-            let i = 0;
+        getElement(child)
 
-            console.log(children[1])
-            getElement(child)
-            if (child.localName === 'li') {
+    let i = 0;
+        if (child.localName !== 'ol' && child.localName === 'li' ) {
 
-                child.classList.add('not_active');
+        child.classList.add('not_active');
 
-                next_btn.onclick = () => {
-                    child.classList.replace('not_active', 'active');
-                }
+        console.log(child)
 
-            }
+        next_btn.onclick = () => {
+
+            children[i]?.classList?.replace('not_active', 'active');
+            i++
         }
+        back_btn.onclick = () => {
+            children[i]?.classList.replace('active', 'not_active');
+            i--
+        }
+
     }
 
-
-
+    }
 }
 
 getElement(document.body);
 
+    // next_btn.onclick = () => {
+    //     children[i]?.classList?.replace('not_active', 'active');
+    //     i++
+    // }
+    // back_btn.onclick = () => {
+    //     children[i]?.classList.replace('active', 'not_active');
+    //     i--
+    // }
+
+    // if (child.localName !== 'ol' && child.localName === 'li' ) {
+    //
+    //     child.classList.add('not_active');
+    //     console.log(children[i])
+    //
+    //     next_btn.onclick = () => {
+    //
+    //         children[i]?.classList?.replace('not_active', 'active');
+    //         i++
+    //     }
+    //     back_btn.onclick = () => {
+    //         children[i]?.classList.replace('active', 'not_active');
+    //         i--
+    //     }
+    //
+    // } else if (child.localName === 'ol') {
+    //     child.classList.remove('not_active', 'active');
+    // }
